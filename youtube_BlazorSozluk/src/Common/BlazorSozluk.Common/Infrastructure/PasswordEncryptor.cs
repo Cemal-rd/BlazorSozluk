19f6a8f7ff69f2769746bde077d3a1d850db0a1f
Cemal Ordu <rdcemal@gmail.com>
2024-06-10 05:42:06 +0300

    Rabbitmq UserCreateUpdate done

---
+++ youtube_BlazorSozluk/src/Common/BlazorSozluk.Common/Infrastructure/PasswordEncryptor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.Infrastructure;
+public class PasswordEncryptor
+{
+    public static string Encrpt(string password)
+    {
+        using var sha256 = SHA256.Create();
+
+        byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+        byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+    }
+}
