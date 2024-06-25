2fadb40502831a6c01ada707f08ccd7b9298eeb2
Cemal Ordu <rdcemal@gmail.com>
2024-06-25 03:05:42 +0300

    All Queries Done

---
+++ youtube_BlazorSozluk/src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/SearchBySubject/SearchEntryQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.SearchBySubject;
+
+public class SearchEntryQueryHandler : IRequestHandler<SearchEntryQuery, List<SearchEntryViewModel>>
+{
+    private readonly IEntryRepository entryRepository;
+
+    public SearchEntryQueryHandler(IEntryRepository entryRepository)
+    {
+        this.entryRepository = entryRepository;
+    }
+
+    public async Task<List<SearchEntryViewModel>> Handle(SearchEntryQuery request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.SearchText) || request.SearchText.Length < 3 || request.SearchText.Length > 25)
+        {
+            throw new ArgumentException("SearchText en az 3, en fazla 25 karakter uzunluğunda olmalıdır.");
+        }
+
+        var result = entryRepository
+            .Get(i => EF.Functions.Like(i.Subject, $"{request.SearchText}%"))
+            .Select(i => new SearchEntryViewModel()
+            {
+                Id = i.Id,
+                Subject = i.Subject,
+            });
+
+        return await result.ToListAsync(cancellationToken);
+    }
+}
